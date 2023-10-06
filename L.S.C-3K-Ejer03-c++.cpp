674d8293eb05ec8c384cfbf7d2f95a0bba799c68
Pao-Esquinca <pl836507@gmail.com>
2023-10-05 18:13:03 -0600

    Agregar archivos de proyecto.

---
+++ L.S.C-3K-Ejer03-c++.cpp
@@ -0,0 +1,20 @@
+// L.S.C-3K-Ejer03-c++.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include<iostream>
+using namespace std;
+int main() {
+	int numeros[6] = { 10,20,30,40,50,60 };
+	cout << numeros[5] << endl;
+
+	int n[3] = { 3,4,5 };
+	cout << n[2] << endl;
+
+	char c[] = { 'L','u','i','s' };
+	cout << c << endl;
+
+	char s[] = "Mona Lisa";
+	cout << s << endl;
+
+	return 0;
+}
